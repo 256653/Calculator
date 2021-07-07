5ee52089aa36c2233c4a002a4b566524ee9eb553
256653 <hemanth.1si17ec029@gmail.com>
2021-07-07 15:18:24 +0530

    changes

---
+++ 3_Implementation/src/addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2,num3;
+    printf("Enter three numbers:\n");
+    scanf("%d%d%d",&num1,&num2,&num3);
+    printf("\nThe sum is %d\n",num1+num2+num3);
+    return 0;
+}
